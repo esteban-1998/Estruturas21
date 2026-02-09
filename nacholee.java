7ad91b61cf97a310fdd4a5ae55bcfd1092de8283
esteban morales valderrama <estebanmorales1116339@correo.itm.edu.co>
2026-02-09 09:12:48 -0500

    nacho lee

--- nacholee.java
+++ nacholee.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class nacholee {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("vamos a sumar");
+        int n1 = 0, n2 = 1;
+        Scanner sc =new Scanner(System.in);
+        System.out.println("ingrese el perimer numero:");
+        n1=sc.nextInt();
+        System.out.println("ingrese el segundo numero:");
+        n2=sc.nextInt();
+        System.out.println("esta es la suma:");
+
+
     }
     
 }
